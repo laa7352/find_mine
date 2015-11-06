3f394bb2e2a702250c8d4c324e858b685a240efc
Jimmy Lin <laa7352@hotmail.com>
2015-11-06 18:51:43 +0800

    Add curses library, and implement MineWindow.
    Signed-off-by: Jimmy Lin <laa7352@hotmail.com>

--- Makefile
+++ Makefile
@@ -6,6 +6,6 @@ clean:
 	rm -rf find_mine
 
 find_mine := find_mine.c
-find_mine: 
+find_mine: find_mine.c
 	gcc $(find_mine) $(CFLAG) -o $@
 
--- find_mine.c
+++ find_mine.c
@@ -2,72 +2,20 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <curses.h>
+#define MineFlag 0x0001
+#define CleanFlag 0x0002
+#define MarkFlag 0x0003
+
 
 void random(void){
   srand(time(NULL));
 }
 
-
-int main(void){
-  int i,j,k;
-  int ix=10,iy=10;
-  int imine=10;
-  int MineMap [ix*iy];
-  int MineList [imine];
- // memset(MineMap, 0, ix*iy*sizeof(int));
-
-// Create random seed
-  random();
-
-// Initial MineMap
-  for (i=0; i<ix*iy; i++){
-    MineMap[i]=i;
-  }
-
-//  printf("test\nMineMap: ");
-//  for (k=0; k<ix*iy ; k++){
-//    printf("%d,", MineMap[k]);
-//  }
-//  printf("\n");
+#if 0
+void printMap(int * MM, int ix, int iy){
+  int i,j;
  
-// Random create MineList
-  for (i=0; i<imine ; i++){
-    int irand=rand()%(ix*iy-i);
-    int temp=MineMap[irand];
-    MineList[i]=temp;
-//    printf("Random number 1-400: %d\n",irand);
-//    printf("Random x, y: %d, %d\n", temp/ix, temp%iy);
-//    printf("Random ix*iy-1: %d, (ix*iy-1)-i: %d, temp: %d, irand: %d\n" , ix*iy-i, (ix*iy-1)-i, temp, irand);
-    for(j=irand; j<(ix*iy-1)-i; j++){
-       MineMap[j]=MineMap[j+1];
-    }
-    MineMap[ix*iy-1-i]=temp;
-//    printf("test\nMineMap: ");
-//    for (k=0; k<ix*iy ; k++){
-//      printf("%d,", MineMap[k]);
-//    }
-//    printf("\n\n");
-  }
-
-// Initial MineMap with 0
-  for (i=0; i<ix*iy; i++){
-    MineMap[i]=0;
-  }
-  for (i=0; i<imine; i++){
-    MineMap[MineList[i]]=1;
-  }
-
-
-//  printf("test\nMineMap: ");
-//  for (k=0; k<ix*iy ; k++){
-//    printf("%d,", MineMap[k]);
-//  }
-//  printf("\n");
-
-
-// Print MineMap
-
-
   printf(" \\ ");
   for(i=0; i<ix; i++){
     printf(" -");
@@ -78,10 +26,10 @@ int main(void){
   for(j=0; j<iy; j++){
     printf(" | ");
     for(i=0; i<ix; i++){
-      if(MineMap[j*10+i] == 1){
+      if(MM[j*iy+i]&MineFlag ){
         printf(" B");
       }else{
-        printf(" #");
+        printf("  ");
       }
     }
     printf("  | ");
@@ -94,6 +42,108 @@ int main(void){
   }
   printf("  \\ ");
   printf("\n");
-  
+
+}
+#endif
+
+void CreateMineMap(int * MineMap, int MapSize, int * MineList, int MineN){
+  int i,j;
+  //int sum=0;
+
+// Create random seed
+  random();
+
+// Initial MineMap
+  for (i=0; i<MapSize; i++){
+    MineMap[i]=i;
+  }
+
+// Random create MineList
+  for (i=0; i<MineN ; i++){
+    int irand=rand()%(MapSize-i);
+    int temp=MineMap[irand];
+    MineList[i]=temp;
+    for(j=irand; j<(MapSize-1)-i; j++){
+       MineMap[j]=MineMap[j+1];
+    }
+    MineMap[MapSize-1-i]=temp;
+  }
+
+// Initial MineMap with 0, Mine set as MineFlag
+  for (i=0; i<MapSize; i++){
+    MineMap[i]=0;
+  }
+  for (i=0; i<MineN; i++){
+    MineMap[MineList[i]]=MineMap[MineList[i]]|MineFlag;
+//    printf("x: %d, y: %d, %d\n", MineList[i]%20, MineList[i]/20, MineMap[MineList[i]]);
+ //   sum++;
+
+  }
+//  printf("sum: %d\n",sum);
+ // getch();
+}
+
+void init_window(){
+  initscr(); //  ←┐
+  cbreak;    //    │ 啟動 curses 模式
+  noecho();  //    │
+  nonl();    //  ←┘
+  refresh();
+}
+
+void Create_MineWin(WINDOW * MineWin, int iy, int ix, int * MineMap){
+  int i,j,mx,my;
+
+  MineWin=newwin(iy+2, 2*(ix+1)+1, 0, 0);
+  box(MineWin, '|', '-');
+  touchwin(MineWin);
+  wrefresh(MineWin);
+#if 1
+  for(j=0; j<iy; j++){
+    //sleep(1);
+//    printf(" | ");
+    for(i=0; i<ix; i++){
+      my=j+1;
+      mx=2*(i+1);
+      wmove(MineWin, my, mx);
+      if(*(MineMap+j*iy+i)&MineFlag ){
+        waddch(MineWin, 'B');
+      }else{
+        waddch(MineWin, '#');
+      //  printf("  ");
+      }
+    }
+ //   printf("  | ");
+  //  printf("\n");
+  }
+#endif
+  wrefresh(MineWin);
+}
+
+int main(void){
+  int ix=20,iy=20;
+  int imine=10;
+  int MineMap [ix*iy];
+  int MineList [imine];
+  WINDOW *MineWin;
+
+ // memset(MineMap, 0, ix*iy*sizeof(int));
+  CreateMineMap(MineMap, ix*iy, MineList, imine);
+
+// Print MineMap
+  init_window();
+  Create_MineWin(MineWin, iy, ix, MineMap);
+#if 0
+  MineWin=newwin(iy+2, 2*(ix+2)+1, 0, 0);
+  box(MineWin, '|', '-');
+  touchwin(MineWin);
+  wrefresh(MineWin);
+  getch();
+#endif
+
+  getch();
+  endwin();
+
+//  printMap(MineMap, ix, iy);
   return 0;
 }
