787bde586da2ddd0ae820adb0aeab59b42d25f6c
Jimmy Lin <laa7352@hotmail.com>
2015-11-08 23:00:20 +0800

    Bug: 15x10 Check_MineWin can check all map
    Signed-off-by: Jimmy Lin <laa7352@hotmail.com>

--- find_mine.c
+++ find_mine.c
@@ -77,7 +77,7 @@ void CreateMineMap(unsigned int * MineMap, int MapSize, int * MineList, int Mine
 void init_window(){
   initscr(); //  ←┐
   cbreak;    //    │ 啟動 curses 模式
-  noecho();  //    │
+//  noecho();  //    │
   nonl();    //  ←┘
   intrflush(stdscr,FALSE);
   keypad(stdscr,TRUE);
@@ -99,8 +99,7 @@ void Create_MineWin(WINDOW ** MineWin, int iy, int ix, unsigned int * MineMap, i
       waddch(*MineWin, '#');
     }
   }
-#endif
-  wrefresh(*MineWin);
+//  wrefresh(*MineWin);
 }
 
 void Check_MineWin(WINDOW * MineWin, int iy, int ix, unsigned int * MineMap, int cy, int cx){
@@ -108,25 +107,28 @@ void Check_MineWin(WINDOW * MineWin, int iy, int ix, unsigned int * MineMap, int
 
   touchwin(MineWin);
   wrefresh(MineWin);
-  if(*(MineMap+cy*iy+cx)&MineFlag ){
-    wmove(MineWin, (cy+1), 2*(cx+1));
-    waddch(MineWin, 'B');
-  }else{
-    wmove(MineWin, (cy+1), 2*(cx+1));
-    *(MineMap+cy*iy+cx)|=CleanFlag;
-    for(i=(cx>0?cx-1:0);i<=(cx<ix-1?cx+1:ix-1);i++){
-    for(j=(cy>0?cy-1:0);j<=(cy<iy-1?cy+1:iy-1);j++){
-      if(*(MineMap+j*iy+i)&MineFlag ) cm++;
-    }}
-    if(cm >0 ){
+  if(!(*(MineMap+cy*iy+cx)&MarkFlag) ){
+    if(*(MineMap+cy*iy+cx)&MineFlag ){
+      *(MineMap+cy*iy+cx)|=CleanFlag;
       wmove(MineWin, (cy+1), 2*(cx+1));
-      waddch(MineWin, cm+48);  // In ACSII table, number 0 is '48'.
+      waddch(MineWin, 'B');
     }else{
-      waddch(MineWin, ' ');
+      wmove(MineWin, (cy+1), 2*(cx+1));
+      *(MineMap+cy*iy+cx)|=CleanFlag;
       for(i=(cx>0?cx-1:0);i<=(cx<ix-1?cx+1:ix-1);i++){
       for(j=(cy>0?cy-1:0);j<=(cy<iy-1?cy+1:iy-1);j++){
-        if(!(*(MineMap+j*iy+i)&CleanFlag)) Check_MineWin(MineWin, iy, ix, MineMap, j, i);
+        if(*(MineMap+j*iy+i)&MineFlag ) cm++;
       }}
+      if(cm >0 ){
+        wmove(MineWin, (cy+1), 2*(cx+1));
+        waddch(MineWin, cm+48);  // In ACSII table, number 0 is '48'.
+      }else{
+        waddch(MineWin, ' ');
+        for(i=(cx>0?cx-1:0);i<=(cx<ix-1?cx+1:ix-1);i++){
+        for(j=(cy>0?cy-1:0);j<=(cy<iy-1?cy+1:iy-1);j++){
+          if(!(*(MineMap+j*iy+i)&CleanFlag)) Check_MineWin(MineWin, iy, ix, MineMap, j, i);
+        }}
+      }
     }
   }
 }
@@ -137,7 +139,7 @@ void Mark_MineWin(WINDOW * MineWin, int iy, int ix, unsigned int * MineMap, int 
   touchwin(MineWin);
   wrefresh(MineWin);
 
-  if(!(*(MineMap+my*iy+mx)&MarkFlag) & !(*(MineMap+my*iy+mx)&CleanFlag)){
+  if(!(*(MineMap+my*iy+mx)&MarkFlag) & !(*(MineMap+my*iy+mx)&CleanFlag) ){
     *(MineMap+my*iy+mx)|=MarkFlag;
     wmove(MineWin, (my+1), 2*(mx+1));
     waddch(MineWin, '?');
@@ -204,21 +206,105 @@ void Move_MineWin(WINDOW * MineWin, int iy, int ix, unsigned int * MineMap){
   }
 }
 
+int Action_MenuWin(int * level, WINDOW ** MenuWin){
+  int mx=3,my=2;
+  int action=1;
+  int ch;
+
+  *MenuWin=newwin(6, 40, LINES/2-3, COLS/2-20);
+  box(*MenuWin, '|', '-');
+  mvwaddstr(*MenuWin,1,1,"Please choose the following mode.");
+  mvwprintw(*MenuWin,2,1," <*> : %dx%d grids, %d mines", *level, *(level+1), *(level+2));
+  mvwprintw(*MenuWin,3,1,"  *  : %dx%d grids, %d mines", *(level+3), *(level+3+1), *(level+3+2));
+  mvwprintw(*MenuWin,4,1,"  *  : %dx%d grids, %d mines", *(level+6), *(level+6+1), *(level+6+2));
+  wmove(*MenuWin, my, mx);
+  touchwin(*MenuWin);
+  wrefresh(*MenuWin);
+
+  while(action){
+    ch=getch();
+    switch (ch){
+      case KEY_UP:
+        mvwaddstr(*MenuWin,my,1,"  * ");
+        my=(my>2?my-1:4);
+        mvwaddstr(*MenuWin,my,1," <*>");
+//        my--;
+
+        wmove(*MenuWin, my, mx);
+    //    if( my > 0 )my--;
+        break;
+      case KEY_DOWN:
+        mvwaddstr(*MenuWin,my,1,"  * ");
+        my=((my<4)?my+1:2);
+        mvwaddstr(*MenuWin,my,1," <*>");
+        //my++;
+
+        wmove(*MenuWin, my, mx);
+     //   if( my < iy-1 )my++;
+        break;
+      case 32:  // space
+//        Check_MineWin(MineWin, iy, ix, MineMap, my, mx);
+/*        if(*(MineMap+my*iy+mx)&MineFlag ){
+          waddch(MineWin, 'B');
+        }else{
+          waddch(MineWin, ' ');
+        }*/
+        break;
+      case '\r':
+        return my-2;
+        break;
+      case 'q':
+        action=0;
+        break;
+      default:
+        break;
+    }
+    touchwin(*MenuWin);
+    wrefresh(*MenuWin);
+  }
+/*  for(j=0; j<iy; j++){
+    for(i=0; i<ix; i++){
+      my=j+1;
+      mx=2*(i+1);
+      wmove(*MineWin, my, mx);
+      waddch(*MineWin, '#');
+    }
+  }*/
+  touchwin(stdscr);
+  return 1;
+}
+
 int main(void){
-  int ix=10,iy=10;
+  int ix=16,iy=15;
   int imine=10;
+  int level[3][3] ={{10, 10, 10},{15, 15, 40},{30, 15, 100}};
   unsigned int MineMap [ix*iy];
-  int MineList [imine];
-  WINDOW *MineWin;
+  int MineList [imine], ilevel;
+  WINDOW *MineWin, *MenuWin;
+
+
+//     win[0]=newwin(LINES/2-1,COLS-1,0,0);       /* 設定兩個視窗的大小*/
+//     win[1]=newwin(LINES/2-1,COLS-1,LINES/2,0);
 
  // memset(MineMap, 0, ix*iy*sizeof(int));
-  CreateMineMap(MineMap, ix*iy, MineList, imine);
 
 // Print MineMap
   init_window();
-  Create_MineWin(&MineWin, iy, ix, MineMap, 1, 1);
 
+//  ilevel=Action_MenuWin(level, &MenuWin);
+ // printf("ilevel: %d, ix: %d, iy: %d, imine: %d\n",ilevel, level[ilevel][0], level[ilevel][1], level[ilevel][2]);
+
+  refresh();
+#if 0
+  CreateMineMap(MineMap, level[ilevel][1]*level[ilevel][0], MineList, level[ilevel][2]);
+  Create_MineWin(&MineWin, level[ilevel][1], level[ilevel][0], MineMap, 1, 1);
+  Move_MineWin(MineWin, level[ilevel][1], level[ilevel][0], MineMap); 
+#else
+
+  CreateMineMap(MineMap, ix*iy, MineList, imine);
+  Create_MineWin(&MineWin, iy, ix, MineMap, 1, 1);
   Move_MineWin(MineWin, iy, ix, MineMap); 
+#endif
 
   endwin();
 
